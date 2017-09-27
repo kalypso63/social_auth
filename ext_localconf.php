dbf98e210ebef4bcb1d49ac46b6d6dc1140a369e
VANCLOOSTER Mickael <vanclooster.mickael@gmail.com>
2017-09-27 18:47:44 +0200

    Bug fixe with Chash

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,27 +3,37 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-
 $composerAutoloadFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/Librairies/autoload.php';
 if (file_exists($composerAutoloadFile)) {
     require_once($composerAutoloadFile);
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'MV.' . $_EXTKEY,
-    'Pi1',
-    array(
-        'Auth' => 'list, connect, endpoint',
-    ),
-    // non-cacheable actions
-    array(
-        'Auth' => 'connect, endpoint'
-    )
-);
+if (TYPO3_MODE === 'FE') {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'MV.' . $_EXTKEY,
+        'Pi1',
+        array(
+            'Auth' => 'list, connect, endpoint',
+        ),
+        // non-cacheable actions
+        array(
+            'Auth' => 'connect, endpoint'
+        )
+    );
+    $extConfig = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['social_auth']);
+    if ($extConfig['providers.']['facebook.']['enabled'] || $extConfig['providers.']['google.']['enabled'] || $extConfig['providers.']['twitter.']['enabled']) {
+        $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] = true;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing'][$_EXTKEY] = 'MV\SocialAuth\Hooks\LogOffHook->postProcessing';
+        //add marker to felogin if is loaded
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('felogin')) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['postProcContent'][$_EXTKEY] = 'MV\SocialAuth\Hooks\FeLoginHook->postProcContent';
+        }
+    }
+}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY,
     'auth' /* sv type */,
-    'SocialAuthenticationService' /* sv key */,
+    MV\SocialAuth\Service\SocialAuthenticationService::class /* sv key */,
     array(
         'title' => 'Social Authentification Service',
         'description' => 'authentication for users from social providers (facebook, twitter...)',
@@ -33,18 +43,10 @@ if (file_exists($composerAutoloadFile)) {
         'quality' => 82,
         'os' => '',
         'exec' => '',
-        'className' => 'MV\\SocialAuth\\Service\\SocialAuthenticationService',
+        'className' => MV\SocialAuth\Service\SocialAuthenticationService::class,
     )
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] = true;
-
-$extConfig = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['social_auth']);
-
-if ($extConfig['providers.']['facebook.']['enabled'] || $extConfig['providers.']['google.']['enabled'] || $extConfig['providers.']['twitter.']['enabled']) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing'][$_EXTKEY] = 'MV\SocialAuth\Hooks\LogOffHook->postProcessing';
-    //add marker to felogin if is loaded
-    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('felogin')) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['postProcContent'][$_EXTKEY] = 'MV\SocialAuth\Hooks\FeLoginHook->postProcContent';
-    }
-}
+#Exclude some params
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_socialauth_pi1[provider]';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_socialauth_pi1[redirect]';
